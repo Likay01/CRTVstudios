50f7f064850714cf9df97bb50a6e4581a41d2838
Likay01 <mgoqilindokuhle2gmail.com>
2023-03-17 11:51:28 +0200

    done

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ const cookieParser = require('cookie-parser');
 app.use((req, res, next)=> {
     res.header('Access-Control-Allow-Origin', '*')
     res.header("Access-Control-Allow-Credentials", "true")
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE")
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-Width, Content-Type, Accept")
+    res.header("Access-Control-Allow-Methods", "*")
+    res.header("Access-Control-Allow-Headers", "*")
     next();
 });
 app.use(route);
